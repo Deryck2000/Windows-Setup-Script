c85edf27df8b07fa129a4c955636e6845295536d
Deryck2000 <90670700+Deryck2000@users.noreply.github.com>
2025-01-15 18:38:51 +0900

    o

--- sources/setup.ps1
+++ sources/setup.ps1
@@ -30,6 +30,7 @@ Start-Sleep -Seconds 1
 # ユーザーフォルダに移動 
 Set-Location -Path $env:USERPROFILE
 
+# セットアップの選択肢を表示
 function SelectJob {
     while ($true) {
         $input = Read-Host "セットアップを選択してください"
@@ -42,273 +43,253 @@ function SelectJob {
     }
 }
 
-
-
-
-
-
-
 # ------------------------------------------------------------------------------
 # レジストリ編集
 # ------------------------------------------------------------------------------
 
 function Job1 {
-Write-Host "レジストリを編集します..."
+    Write-Host "レジストリを編集します..."
+    Start-Sleep -Seconds 1
+    Write-Host " "
 
-Start-Sleep -Seconds 1
-Write-Host " "
+    # エクスプローラで拡張子を表示
+    Write-Host "レジストリを編集:エクスプローラで拡張子を表示..."
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0
 
-# エクスプローラで拡張子を表示
-Write-Host "レジストリを編集:エクスプローラで拡張子を表示..."
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0
+    # エクスプローラで隠しファイルを表示
+    Write-Host "レジストリを編集:エクスプローラで隠しファイルを表示..."
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Hidden' -Value 1
 
-# エクスプローラで隠しファイルを表示
-Write-Host "レジストリを編集:エクスプローラで隠しファイルを表示..."
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Hidden' -Value 1
+    # エクスプローラでコンパクトビューを使用する
+    Write-Host "レジストリを編集:エクスプローラでコンパクトビューを使用する..."
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'UseCompactMode' -Value 1
 
-# エクスプローラでコンパクトビューを使用する
-Write-Host "レジストリを編集:エクスプローラでコンパクトビューを使用する..."
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'UseCompactMode' -Value 1
+    # Windows をダークモードに設定
+    Write-Host "レジストリを編集:Windows をダークモードに設定..."
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'AppsUseLightTheme' -Value 0
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'SystemUsesLightTheme' -Value 0
 
-# Windows をダークモードに設定
-Write-Host "レジストリを編集:Windows をダークモードに設定..."
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'AppsUseLightTheme' -Value 0
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'SystemUsesLightTheme' -Value 0
+    # クリップボード履歴の有効化
+    Write-Host "レジストリを編集:クリップボード履歴の有効化..."
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Clipboard' -Name 'EnableClipboardHistory' -Value 1
 
-# クリップボード履歴の有効化
-Write-Host "レジストリを編集:クリップボード履歴の有効化..."
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Clipboard' -Name 'EnableClipboardHistory' -Value 1
+    # マウスのプロパティの「ポインターの精度を高める」を無効化
+    Write-Host "レジストリを編集:マウスのプロパティの「ポインターの精度を高める」を無効化..."
+    Set-ItemProperty -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseSpeed' -Value 0
+    Set-ItemProperty -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseThreshold1' -Value 0
+    Set-ItemProperty -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseThreshold2' -Value 0
 
-# マウスのプロパティの「ポインターの精度を高める」を無効化
-Write-Host "レジストリを編集:マウスのプロパティの「ポインターの精度を高める」を無効化..."
-Set-ItemProperty -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseSpeed' -Value 0
-Set-ItemProperty -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseThreshold1' -Value 0
-Set-ItemProperty -Path 'HKCU:\Control Panel\Mouse' -Name 'MouseThreshold2' -Value 0
+    Write-Host " "
+    Write-Host "レジストリの編集を完了しました。" -ForegroundColor Green
+    Start-Sleep -Seconds 1
 
-Write-Host " "
-Write-Host "レジストリの編集を完了しました。" -ForegroundColor Green
-Start-Sleep -Seconds 1
+    # ------------------------------------------------------------------------------
+    # DNS設定の変更
+    # ------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------
-# DNS設定の変更
-# ------------------------------------------------------------------------------
+    Write-Host " "
+    Write-Host "パブリックDNSをGoogleのものに変更します。"
+    Start-Sleep -Seconds 1
 
-Write-Host " "
-Write-Host "パブリックDNSをGoogleのものに変更します。"
-Start-Sleep -Seconds 1
+    $interfaceIndex = (Get-NetIPInterface -AddressFamily IPv4).InterfaceIndex
+    Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses 8.8.8.8
 
-$interfaceIndex = (Get-NetIPInterface -AddressFamily IPv4).InterfaceIndex
-Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses 8.8.8.8
+    Write-Host "パブリックDNSを変更しました。" -ForegroundColor Green
+    Start-Sleep -Seconds 1
 
-Write-Host "パブリックDNSを変更しました。" -ForegroundColor Green
-Start-Sleep -Seconds 1
-
-Selecter
-
-
+    Selecter
 }
+
 # ------------------------------------------------------------------------------
 # パッケージのインストール
 # ------------------------------------------------------------------------------
 
 function Job2 {
-Write-Host " "
-Write-Host "WinGetがインストールされているかチェックします..."
+    Write-Host " "
+    Write-Host "WinGetがインストールされているかチェックします..."
 
-# WinGet がインストールされているかをチェックする
-function Test-WinGet {
-    $chocoPath = (Get-Command winget -ErrorAction SilentlyContinue).Path
-    return -not [string]::IsNullOrEmpty($chocoPath)
-}
-
-if (Test-WinGet) {
-
-    Write-Host "WinGetはインストールされています。" -ForegroundColor Green
-
-}else{
-
-    Write-Host "WinGetをインストールします。"
-    Start-Sleep -Seconds 1
-    $progressPreference = 'silentlyContinue'
-    Write-Host "Installing WinGet PowerShell module from PSGallery..."
-    Install-PackageProvider -Name NuGet -Force | Out-Null
-    Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
-    Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
-    Repair-WinGetPackageManager
-    Write-Host "WinGetをインストールしました。"  -ForegroundColor Green
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-    Write-Host "環境変数を再読み込みしました。"  -ForegroundColor Green
-}
-
-Start-Sleep -Seconds 1
-Write-Host " "
-
-Write-Host "WinGetを使用してパッケージをインストールします。"
-Start-Sleep -Seconds 1
-
-# 実行したい別のスクリプトのパスを設定する
-$packageTxt = Join-Path -Path $PSScriptRoot -ChildPath "packages.txt"
-
-# インストールするパッケージのリストをファイルから読み込み
-$packageList = Get-Content -Path $packageTxt
-
-# インストールに失敗したパッケージを保持する配列
-$failedInstalls = @()
-
-Write-Host " "
-Write-Host "インストールするパッケージリスト:" 
-$packageList | ForEach-Object { Write-Host $_ }
-Start-Sleep -Seconds 1
-
-Write-Host " "
-Write-Host "インストールを開始します。"
-Start-Sleep -Seconds 1
-
-foreach ($software in $packageList) {
-    Write-Host "$software をインストールします..."
-    winget install -e --id $software
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host "$software のインストールに失敗しました。" -ForegroundColor Red
-        $failedInstalls += $software
-        Start-Sleep -Seconds 1
-        Write-Host " "
-    } else {
-        Write-Host "$software を正常にインストールしました。" -ForegroundColor Green
-        Start-Sleep -Seconds 1
-        Write-Host " "
+    # WinGet がインストールされているかをチェックする
+    function Test-WinGet {
+        $chocoPath = (Get-Command winget -ErrorAction SilentlyContinue).Path
+        return -not [string]::IsNullOrEmpty($chocoPath)
     }
+
+    if (Test-WinGet) {
+        Write-Host "WinGetはインストールされています。" -ForegroundColor Green
+    } else {
+        Write-Host "WinGetをインストールします。"
+        Start-Sleep -Seconds 1
+        $progressPreference = 'silentlyContinue'
+        Write-Host "Installing WinGet PowerShell module from PSGallery..."
+        Install-PackageProvider -Name NuGet -Force | Out-Null
+        Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+        Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+        Repair-WinGetPackageManager
+        Write-Host "WinGetをインストールしました。"  -ForegroundColor Green
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+        Write-Host "環境変数を再読み込みしました。"  -ForegroundColor Green
+    }
+
+    Start-Sleep -Seconds 1
+    Write-Host " "
+    Write-Host "WinGetを使用してパッケージをインストールします。"
+    Start-Sleep -Seconds 1
+
+    # 実行したい別のスクリプトのパスを設定する
+    $packageTxt = Join-Path -Path $PSScriptRoot -ChildPath "packages.txt"
+
+    # インストールするパッケージのリストをファイルから読み込み
+    $packageList = Get-Content -Path $packageTxt
+
+    # インストールに失敗したパッケージを保持する配列
+    $failedInstalls = @()
+
+    Write-Host " "
+    Write-Host "インストールするパッケージリスト:" 
+    $packageList | ForEach-Object { Write-Host $_ }
+    Start-Sleep -Seconds 1
+
+    Write-Host " "
+    Write-Host "インストールを開始します。"
+    Start-Sleep -Seconds 1
+
+    foreach ($software in $packageList) {
+        Write-Host "$software をインストールします..."
+        winget install -e --id $software
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host "$software のインストールに失敗しました。" -ForegroundColor Red
+            $failedInstalls += $software
+            Start-Sleep -Seconds 1
+            Write-Host " "
+        } else {
+            Write-Host "$software を正常にインストールしました。" -ForegroundColor Green
+            Start-Sleep -Seconds 1
+            Write-Host " "
+        }
+    }
+
+    Start-Sleep -Seconds 1
+
+    # 失敗したパッケージを出力
+    if ($failedInstalls.Count -gt 0) {
+        Write-Host "これらのパッケージのインストールに失敗しました:" -ForegroundColor Red
+        $failedInstalls | ForEach-Object { Write-Host $_ }
+    } else {
+        Write-Host "すべてのパッケージを正常にインストールしました。" -ForegroundColor Green
+    }
+
+    Start-Sleep -Seconds 1
+    Selecter
 }
 
-Start-Sleep -Seconds 1
-
-# 失敗したパッケージを出力
-if ($failedInstalls.Count -gt 0) {
-    Write-Host "これらのパッケージのインストールに失敗しました:" -ForegroundColor Red
-    $failedInstalls | ForEach-Object { Write-Host $_ }
-} else {
-    Write-Host "すべてのパッケージを正常にインストールしました。" -ForegroundColor Green
-}
-
-
-Start-Sleep -Seconds 1
-Selecter
-
-}
-
+# ------------------------------------------------------------------------------
+# エクストラ・パッケージのインストール
+# ------------------------------------------------------------------------------
 
 function Job3 {
+    Write-Host " "
+    Write-Host "WinGetがインストールされているかチェックします..."
 
-Write-Host " "
-Write-Host "WinGetがインストールされているかチェックします..."
-
-# WinGet がインストールされているかをチェックする
-function Test-WinGet {
-    $chocoPath = (Get-Command winget -ErrorAction SilentlyContinue).Path
-    return -not [string]::IsNullOrEmpty($chocoPath)
-}
-
-if (Test-WinGet) {
-
-    Write-Host "WinGetはインストールされています。" -ForegroundColor Green
-
-}else{
-
-    Write-Host "WinGetをインストールします。"
-    Start-Sleep -Seconds 1
-    $progressPreference = 'silentlyContinue'
-    Write-Host "Installing WinGet PowerShell module from PSGallery..."
-    Install-PackageProvider -Name NuGet -Force | Out-Null
-    Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
-    Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
-    Repair-WinGetPackageManager
-    Write-Host "WinGetをインストールしました。"  -ForegroundColor Green
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-    Write-Host "環境変数を再読み込みしました。"  -ForegroundColor Green
-}
-
-Start-Sleep -Seconds 1
-Write-Host " "
-
-Write-Host "WinGetを使用してパッケージをインストールします。"
-Start-Sleep -Seconds 1
-
-# 実行したい別のスクリプトのパスを設定する
-$packageTxt = Join-Path -Path $PSScriptRoot -ChildPath "packages-ex.txt"
-
-# インストールするパッケージのリストをファイルから読み込み
-$packageList = Get-Content -Path $packageTxt
-
-# インストールに失敗したパッケージを保持する配列
-$failedInstalls = @()
-
-Write-Host " "
-Write-Host "インストールするパッケージリスト:" 
-$packageList | ForEach-Object { Write-Host $_ }
-Start-Sleep -Seconds 1
-
-Write-Host " "
-Write-Host "インストールを開始します。"
-Start-Sleep -Seconds 1
-
-foreach ($software in $packageList) {
-    Write-Host "$software をインストールします..."
-    winget install -e --id $software
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host "$software のインストールに失敗しました。" -ForegroundColor Red
-        $failedInstalls += $software
-        Start-Sleep -Seconds 1
-        Write-Host " "
-    } else {
-        Write-Host "$software を正常にインストールしました。" -ForegroundColor Green
-        Start-Sleep -Seconds 1
-        Write-Host " "
+    # WinGet がインストールされているかをチェックする
+    function Test-WinGet {
+        $chocoPath = (Get-Command winget -ErrorAction SilentlyContinue).Path
+        return -not [string]::IsNullOrEmpty($chocoPath)
     }
+
+    if (Test-WinGet) {
+        Write-Host "WinGetはインストールされています。" -ForegroundColor Green
+    } else {
+        Write-Host "WinGetをインストールします。"
+        Start-Sleep -Seconds 1
+        $progressPreference = 'silentlyContinue'
+        Write-Host "Installing WinGet PowerShell module from PSGallery..."
+        Install-PackageProvider -Name NuGet -Force | Out-Null
+        Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+        Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+        Repair-WinGetPackageManager
+        Write-Host "WinGetをインストールしました。"  -ForegroundColor Green
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+        Write-Host "環境変数を再読み込みしました。"  -ForegroundColor Green
+    }
+
+    Start-Sleep -Seconds 1
+    Write-Host " "
+    Write-Host "WinGetを使用してエクストラ・パッケージをインストールします。"
+    Start-Sleep -Seconds 1
+
+    # 実行したい別のスクリプトのパスを設定する
+    $packageTxt = Join-Path -Path $PSScriptRoot -ChildPath "packages-ex.txt"
+
+    # インストールするパッケージのリストをファイルから読み込み
+    $packageList = Get-Content -Path $packageTxt
+
+    # インストールに失敗したパッケージを保持する配列
+    $failedInstalls = @()
+
+    Write-Host " "
+    Write-Host "インストールするパッケージリスト:" 
+    $packageList | ForEach-Object { Write-Host $_ }
+    Start-Sleep -Seconds 1
+
+    Write-Host " "
+    Write-Host "インストールを開始します。"
+    Start-Sleep -Seconds 1
+
+    foreach ($software in $packageList) {
+        Write-Host "$software をインストールします..."
+        winget install -e --id $software
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host "$software のインストールに失敗しました。" -ForegroundColor Red
+            $failedInstalls += $software
+            Start-Sleep -Seconds 1
+            Write-Host " "
+        } else {
+            Write-Host "$software を正常にインストールしました。" -ForegroundColor Green
+            Start-Sleep -Seconds 1
+            Write-Host " "
+        }
+    }
+
+    Start-Sleep -Seconds 1
+
+    # 失敗したパッケージを出力
+    if ($failedInstalls.Count -gt 0) {
+        Write-Host "これらのパッケージのインストールに失敗しました:" -ForegroundColor Red
+        $failedInstalls | ForEach-Object { Write-Host $_ }
+    } else {
+        Write-Host "すべてのエクストラ・パッケージを正常にインストールしました。" -ForegroundColor Green
+    }
+
+    Start-Sleep -Seconds 1
+    Selecter
 }
-
-Start-Sleep -Seconds 1
-
-# 失敗したパッケージを出力
-if ($failedInstalls.Count -gt 0) {
-    Write-Host "これらのパッケージのインストールに失敗しました:" -ForegroundColor Red
-    $failedInstalls | ForEach-Object { Write-Host $_ }
-} else {
-    Write-Host "すべてのエクストラ・パッケージを正常にインストールしました。" -ForegroundColor Green
-}
-
-
-Start-Sleep -Seconds 1
-Selecter
-
-}
-
 
 # ------------------------------------------------------------------------------
 # 選択
 # ------------------------------------------------------------------------------
 
-function Selecter{
+function Selecter {
+    Write-Host " "
+    Write-Host "------------------------------"
+    Write-Host " "
+    Write-Host "実行するものを選択してください。"
+    Write-Host " "
+    Write-Host "1 : レジストリ編集・パブリックDNSの変更"
+    Write-Host "2 : パッケージのインストール"
+    Write-Host "3 : エクストラ・パッケージのインストール"
+    Write-Host " "
+    $userInput = SelectJob
 
-Write-Host " "
-Write-Host "------------------------------"
-Write-Host " "
-Write-Host "実行するものを選択してください。"
-Write-Host " "
-Write-Host "1 : レジストリ編集・パブリックDNSの変更"
-Write-Host "2 : パッケージのインストール"
-Write-Host "3 : エクストラ・パッケージのインストール"
-Write-Host " "
-$userInput = SelectJob
-
-if ($userInput -eq "1") {
-    Write-Host "レジストリ編集・パブリックDNSの変更 を実行します。"
-    Job1    
-} elseif ($userInput -eq "2") {
-    Write-Host "パッケージのインストール を実行します。"
-    Job2
-} elseif ($userInput -eq "3") {
-    Write-Host "3 : エクストラ・パッケージのインストール を実行します。"
-    Job3
-}
+    if ($userInput -eq "1") {
+        Write-Host "レジストリ編集・パブリックDNSの変更 を実行します。"
+        Job1    
+    } elseif ($userInput -eq "2") {
+        Write-Host "パッケージのインストール を実行します。"
+        Job2
+    } elseif ($userInput -eq "3") {
+        Write-Host "エクストラ・パッケージのインストール を実行します。"
+        Job3
+    }
 }
 
 Selecter
