0f81fd689f93c62dba84324f07764653d3f8d5a7
Deryck2000 <90670700+Deryck2000@users.noreply.github.com>
2025-01-15 15:51:02 +0900

    update

--- Start.bat
+++ Start.bat
@@ -1,3 +1,3 @@
 @echo off
 cd %~dp0
-start powershell -NoProfile -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Unrestricted -File \"%~dp0setup.ps1\"' -Verb RunAs"
+start powershell -NoProfile -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Unrestricted -File \"%~dp0sources\setup.ps1\"' -Verb RunAs"
